a6e3aae90d8041117e3abc44b8508d9fbd69981e
moro <moro.drake@gmail.com>
2021-02-25 21:52:25 +0100

    method listener changed to invocation listener due to problems with multi threading

--- reports/src/test/java/AttachFailureLogsToAllure.java
+++ reports/src/test/java/AttachFailureLogsToAllure.java
@@ -1,25 +1,36 @@
 import io.qameta.allure.Allure;
-import org.testng.ITestListener;
+import io.qameta.allure.AllureLifecycle;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
 import java.util.Iterator;
 import java.util.List;
 
-public class AttachFailureLogsToAllure implements ITestListener {
+public class AttachFailureLogsToAllure implements IInvokedMethodListener {
+    private static final Logger logger = LogManager.getLogger(AttachFailureLogsToAllure.class);
+
 
     @Override
-    public void onTestFailure(ITestResult tr) {
-        Allure.addAttachment("log4j2-logs", addLogsToReport(Reporter.getOutput(tr)));
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (!testResult.isSuccess()) {
+            byte[] attachment = addLogsToReport(Reporter.getOutput(testResult));
+            logger.info("attaching logs to test case");
+            AllureLifecycle lc = Allure.getLifecycle();
+            lc.addAttachment("log4j2-logs", "text/plain", "log", attachment);
+        }
     }
 
-    public static String addLogsToReport(List<String> logs) {
+
+    public static synchronized byte[] addLogsToReport(List<String> logs) {
         StringBuilder stringBuilder = new StringBuilder();
         Iterator<String> logIterator = logs.stream().iterator();
         while (logIterator.hasNext()) {
             stringBuilder.append(logIterator.next());
-            stringBuilder.append(System.lineSeparator());
         }
-        return stringBuilder.toString();
+        return stringBuilder.toString().getBytes();
     }
 }
--- reports/src/test/java/SampleTest.java
+++ reports/src/test/java/SampleTest.java
@@ -1,8 +1,11 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.Reporter;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import static org.testng.Assert.fail;
 
@@ -12,20 +15,65 @@ public class SampleTest {
     @DataProvider(parallel = true)
     public static Object[][] dataLogMsg() {
         return new Object[][]{
-                {"first msg"},
-                {"second msg"},
-                {"third msg"},
+                {"first msg", true},
+                {"second msg", false},
+                {"third msg", true},
+                {"4 msg", false},
+                {"5 msg", false},
+                {"6 msg", false},
+                {"7 msg", false},
+                {"8 msg", false},
+                {"9 msg", false},
+                {"10 msg", false},
+                {"11 msg", false},
+                {"1third msg", true},
+                {"2third msg", true},
+                {"3third msg", true},
+                {"4third msg", true},
+                {"5third msg", true},
+                {"6second msg", false},
+                {"7second msg", false},
+                {"8second msg", false},
+                {"9third msg", true},
+                {"10third msg", true},
+                {"11third msg", true},
+
         };
     }
 
     @Test(dataProvider = "dataLogMsg")
-    public void testCase(String logMsg) {
+    public void testCase(String logMsg, boolean shouldPass) {
         logger.info("running test case with dataLogMsg provider");
         logger.info(logMsg);
+        Assert.assertTrue(shouldPass);
+
     }
+
+    @Test(dataProvider = "dataLogMsg")
+    public void secondCase(String msg, boolean shouldPass) {
+        logger.warn("running test case with dataLogMsg provider");
+        logger.warn(msg);
+        Assert.assertTrue(shouldPass);
+    }
+
+    @Test(dataProvider = "dataLogMsg")
+    public void thirdCase(String msg, boolean shouldPass) {
+        logger.warn("running test case with !dataLogMsg provider");
+        logger.warn(spam.apply(msg));
+        Assert.assertTrue(!shouldPass);
+    }
+
+    Function<String, String> spam = (x) -> x + "asd";
+
+    IntFunction<Integer> doubleIt = x -> x * 2;
+
 
     @Test
     public void testCaseFail() {
+        var a = 9;
+        Long b = Long.valueOf(4);
+        System.out.println(a);
+        System.out.println(b.getClass());
         logger.info("testCaseFail test info log");
         logger.error("testCaseFail test error log");
         logger.warn("testCaseFail test warn log");
